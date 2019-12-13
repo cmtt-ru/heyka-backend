4fa2edf82716123d883835c44de4b399313a532c
Evgenii K <aksyline@gmail.com>
2019-12-13 16:00:42 +0300

    added socket support for the server

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 'use strict';
 
 const createServer = require('./server');
+const socket = require('./lib/socket');
 
 async function startServer () {
   const server = await createServer();
+  const socketIO = await socket.getSocketIO(server);
+  socketIO.attach(server.listener);
   server.start();
   server.log(['info'], 'Server started');
 }

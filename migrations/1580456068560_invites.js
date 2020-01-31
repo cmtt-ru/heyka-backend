54a29fa4cbcfda36731d09a57e60124c46278a12
Evgenii K <aksyline@gmail.com>
2020-01-31 10:49:18 +0300

    add migration for invite codes

---
+++ migrations/1580456068560_invites.js
@@ -0,0 +1,31 @@
+/* eslint-disable camelcase */
+'use strict';
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('invites', {
+    id: {
+      type: 'uuid',
+      primaryKey: true
+    },
+    created_by: {
+      type: 'uuid',
+      references: 'users(id)',
+      onDelete: 'cascade'
+    },
+    workspace_id: {
+      type: 'uuid',
+      references: 'workspaces(id)',
+      onDelete: 'cascade'
+    },
+    code: 'varchar(50)',
+    created_at: 'timestamp',
+    updated_at: 'timestamp',
+    expired_at: 'timestamp'
+  });
+};
+
+exports.down = (pgm) => {
+
+};

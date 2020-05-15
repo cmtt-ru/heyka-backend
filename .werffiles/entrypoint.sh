f99936d06cfcebd9840d990bd928cdc9f6fd9dfd
Ilya Sosnovsky <ilya.sosnovsky@flant.com>
2020-05-15 14:17:56 +0300

    Refactoring entrypoint

--- .werffiles/entrypoint.sh
+++ .werffiles/entrypoint.sh
@@ -2,9 +2,19 @@
 
 export PUBLIC_POD_IP=`curl -f -s http://169.254.169.254/latest/meta-data/public-ipv4`
 
+if [ -z "RTP_PORT_MIN" ]; then
+    echo "ERROR!: you need to setup RTP_PORT_MIN variable"
+    exit 1
+fi
+
+if [ -z "RTP_PORT_MAX" ]; then
+    echo "ERROR!: you need to setup RTP_PORT_MAX variable"
+    exit 1
+fi
+
 
 if [ ! -z "$1" ]; then
   exec "$@"
 else
-  exec /opt/janus/bin/janus --rtp-port-range={{ pluck .Values.global.env .Values.janus.rtp_port.min | first | default .Values.janus.rtp_port.min._default }}-{{ pluck .Values.global.env .Values.janus.rtp_port.max | first | default .Values.janus.rtp_port.max._default }} --nat-1-1=$(PUBLIC_POD_IP)
+  exec /opt/janus/bin/janus --rtp-port-range=${RTP_PORT_MIN}-${RTP_PORT_MAX} --nat-1-1=${PUBLIC_POD_IP}
 fi

3eb03d15bf043f1c7ab663dece41a6e4f563b1f2
Michael Nalbandyan <xinger@ya.ru>
2020-07-29 05:00:32 +0300

    some test landing

--- client/src/views/Landing.vue
+++ client/src/views/Landing.vue
@@ -1,20 +1,57 @@
 <template>
-  <div class="home">
+  <div class="wrapper">
     <img
+      width="200"
       alt="Vue logo"
       src="../assets/logo.png"
     >
-    <HelloWorld />
+    <p class="title">Heyka {{ version }}</p>
+
+    <div class="download">
+      <a href="">macOS</a>
+      <a href="">Windows</a>
+      <a href="">Linux</a>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'Home',
+  data() {
+    return {
+      version: '0.1.1',
+    };
+  },
   components: {
-    HelloWorld,
+
   },
 };
 </script>
+
+<style lang="stylus">
+  body
+    background #F1FAFF
+    line-height 1
+
+  .wrapper
+    max-width 700px
+    margin 70px auto
+    text-align center
+
+    .title
+      font-size 48px
+      font-weight 500
+      margin-top 24px
+      color #151515
+
+  .download
+    margin-top 64px
+
+    a
+      margin 0 6px
+      color #777
+      text-decoration none
+      border-bottom 1px solid #ccc
+</style>

292f6b4746fe6a66c10dcd9f49816624b1ea7485
Michael Nalbandyan <xinger@ya.ru>
2020-08-10 20:35:41 +0300

    work in progress

--- client/src/components/Manage/Workspaces.vue
+++ client/src/components/Manage/Workspaces.vue
@@ -1,13 +1,68 @@
 <template>
-  <h1>test</h1>
+  <div class="workspaces">
+    <div
+      v-for="workspace in workspaces"
+      :key="workspace.id"
+      class="workspaces__item"
+      :class="{'workspaces__item--selected': selectedWorkspace.id === workspace.id}"
+      @click="selectWorkspaceHandler(workspace)"
+    >
+      <img
+        class="logo"
+        width="36"
+        height="36"
+        alt="Vue logo"
+        :src="workspace.avatar"
+      >
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
+  props: {
+    /**
+     * Array of workspace
+     */
+    workspaces: {
+      type: Array,
+      default: function () {
+        return [];
+      },
+    },
+  },
+
+  data() {
+    return {
+      selectedWorkspace: {},
+    };
+  },
+
+  methods: {
+    selectWorkspaceHandler(workspace) {
+      this.selectedWorkspace = workspace;
+      this.$emit('select', workspace);
+    },
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="stylus">
+  .workspaces
+
+    &__item
+      width 46px
+      height 46px
+      border 1px solid #eee
+      padding 4px
+      box-sizing border-box
+      border-radius 4px
+      cursor pointer
+
+      &--selected
+        cursor default
+        border-color #30B1F2
+        background alpha(#30B1F2, 0.1)
 
 </style>

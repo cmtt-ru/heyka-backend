5527ab90fce24411857d6f8715a7aea0c3f9b766
Evgenii K <aksyline@gmail.com>
2020-08-13 12:22:20 +0300

    take public hostname from environment variables

--- config/index.js
+++ config/index.js
@@ -5,7 +5,8 @@ require('dotenv').config();
 module.exports = {
   port: process.env.PORT || 5000,
   host: process.env.HOST || 'localhost',
-  jwt_secret: process.env.JWT_SECRET || 'default_jwt_token',
+  jwtSecret: process.env.JWT_SECRET || 'default_jwt_token',
+  publicHostname: process.env.PUBLIC_HOSTNAME || 'localhost:5000',
   leonardo: {
     uploadUrl: process.env.LEONARDO_UPLOAD_URL || 'https://leonardo-direct.osnova.io/upload/files/',
     staticServerUrl: process.env.LEONARDO_STATIC_SERVER_URL || 'https://leonardo.osnova.io/',

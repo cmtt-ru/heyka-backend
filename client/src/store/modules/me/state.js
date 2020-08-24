805eccf884847ea89b25308df65ef2beaa0462b7
Michael Nalbandyan <xinger@ya.ru>
2020-08-24 23:46:45 +0300

    store me module

---
+++ client/src/store/modules/me/state.js
@@ -0,0 +1,41 @@
+import { meStore } from '@/store/localStore';
+
+/**
+ * @typedef {object} MeState
+ * @property {string} id – my id
+ * @property {string} selectedWorkspaceId – selected workspace id
+ * @property {string} selectedChannelId – selected channel id
+ * @property {MediaState} mediaState – my media state
+ * @property {boolean} allowDraw – Allow/forbid drawing
+ */
+
+/**
+ * Me state
+ * @returns {MeState}
+ */
+const state = () => {
+  /**
+   * @type MediaState
+   */
+  const initialMediaState = {
+    microphone: false,
+    speakers: true,
+    screen: false,
+    camera: false,
+    speaking: false,
+  };
+
+  /**
+   * @namespace MeState
+   */
+  return {
+    id: null,
+    selectedWorkspaceId: meStore.get('selectedWorkspaceId'),
+    selectedChannelId: null,
+    mediaState: initialMediaState,
+    previousMediaState: initialMediaState,
+    allowDraw: true,
+  };
+};
+
+export default state();

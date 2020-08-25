ba9fa762f43c01aa44a63c2cd1c453b11b88b820
Michael Nalbandyan <xinger@ya.ru>
2020-08-25 20:46:58 +0300

    janus files

--- client/src/views/Guest.vue
+++ client/src/views/Guest.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="wrapper">
+    <janus />
     Guest grid
   </div>
 </template>
 
 <script>
+import Janus from '@components/Janus';
+
 const AUTH_CODE = 'be0022025d014923a114ffcaee138b772162c39cfb959d061cf8d2c3eb395ae1e49ea3c2a12576d57b';
 const CHANNEL_ID = '3e6e738c-0317-4037-baf6-0eb8207a5939';
 
 export default {
   name: 'Home',
   components: {
-
+    Janus,
   },
 
   async mounted() {

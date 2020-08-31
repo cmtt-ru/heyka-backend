f9cb2b0091c455ce6fd4c09abc481dbf034cc7c3
Michael Nalbandyan <xinger@ya.ru>
2020-08-31 23:07:37 +0300

    grid initial

--- client/src/store/getters.js
+++ client/src/store/getters.js
@@ -1,3 +1,5 @@
+import { sortAny } from '@libs/arrays';
+
 /**
  * Last speaking user
  * @type {null}
@@ -10,6 +12,32 @@ let lastSpeakingUser = null;
 let lastUserWhoSharesMedia = null;
 
 export default {
+  /**
+   * Get full users in Channel
+   * @param {object} state – channels module state
+   * @param {object} getters – channels module getters
+   * @param {string} id – channel id
+   * @returns {array}
+   */
+  getUsersByChannel: (state, getters) => (id) => {
+    const ch = getters['channels/getChannelById'](id) || { users: [] };
+
+    const users = ch.users.map(user => {
+      return {
+        ...user,
+        ...getters['users/getUserById'](user.userId),
+      };
+    });
+
+    return users.sort(sortAny([
+      {
+        key: 'name',
+        type: 'string',
+        order: 'asc',
+      },
+    ]));
+  },
+
   /**
    * Get user who share's screen or camera
    *

ccc7b01058c89c89a66cc801babc308539a4bdac
Evgenii K <aksyline@gmail.com>
2020-09-11 14:34:37 +0300

    Got rid of stream API, make everything with setTimeout

--- scripts/update_domain_names.js
+++ scripts/update_domain_names.js
@@ -5,7 +5,51 @@ const config = require('../config');
 const cf = require('cloudflare')({
   token: config.cloudflare.dnsAPIKey,
 });
-const JSONStream = require('json-stream');
+const SYNC_INTERVAL = 60 * 1000;
+
+async function syncDomainNamesWithJanusNodes(janusWorkspaceService) {
+  // collect janus nodes
+  const nodes = (await janusWorkspaceService.fetchJanusNodes()).map(node => ({
+    ip: node.status.addresses.find(address => address.type === 'ExternalIP').address
+  }));
+    // collect all dns-records
+  const dnsRecords = (await cf.dnsRecords.browse(config.cloudflare.dnsZoneId)).result
+    .filter(record => record.type === 'A' && record.name.includes('.infr.heyka.io'));
+
+  console.log(`Collected all janus nodes`, dnsRecords);
+
+  // check domain that should be added
+  await Promise.all(
+    nodes.map(async janusNode => {
+      const domainName = janusWorkspaceService.getJanusDomainName(janusNode.ip);
+        
+      // check if domain name should be added
+      if (!dnsRecords.find(record => record.name === domainName)) {
+        await cf.dnsRecords.add(config.cloudflare.dnsZoneId, {
+          type: 'A',
+          name: domainName,
+          content: janusNode.ip,
+          ttl: 120,
+        });
+        console.log(`DNS record for ${janusNode.ip} has been added (${domainName})`);
+      }
+    })
+  );
+
+  console.log(`Added domain name servers`);
+
+  // check domain that should be deleted
+  await Promise.all(
+    dnsRecords.map(async record => {
+      if (!nodes.find(node => node.ip === record.content)) {
+        await cf.dnsRecords.del(config.cloudflare.dnsZoneId, record.id);
+        console.log(`DNS record for ${record.content} has been added (${record.name})`);
+      }
+    })
+  );
+
+  console.log(`Deleted domain name servers`);
+}
 
 async function startWatcher () {
   try {
@@ -17,63 +61,16 @@ async function startWatcher () {
       janusWorkspaceService,
     } = server.services();
 
-    // collect janus nodes
-    const nodes = (await janusWorkspaceService.fetchJanusNodes()).map(node => ({
-      ip: node.status.addresses.find(address => address.type === 'ExternalIP').address
-    }));
-    // collect all dns-records
-    const dnsRecords = (await cf.dnsRecords.browse(config.cloudflare.dnsZoneId)).result
-      .filter(record => record.type === 'A' && record.name.includes('.infr.heyka.io'));
+    await syncDomainNamesWithJanusNodes(janusWorkspaceService);
 
-    console.log(`Collected all janus nodes`, dnsRecords);
-
-    // check domain that should be added
-    await Promise.all(
-      nodes.map(async janusNode => {
-        const domainName = janusWorkspaceService.getJanusDomainName(janusNode.ip);
-        
-        // check if domain name should be added
-        if (!dnsRecords.find(record => record.name === domainName)) {
-          await cf.dnsRecords.add(config.cloudflare.dnsZoneId, {
-            type: 'A',
-            name: domainName,
-            content: janusNode.ip,
-            ttl: 120,
-          });
-          console.log(`DNS record for ${janusNode.ip} has been added (${domainName})`);
-        }
-      })
-    );
-
-    console.log(`Added domain name servers`);
-
-    // check domain that should be deleted
-    await Promise.all(
-      dnsRecords.map(async record => {
-        if (!nodes.find(node => node.ip === record.content)) {
-          await cf.dnsRecords.del(config.cloudflare.dnsZoneId, record.id);
-          console.log(`DNS record for ${record.content} has been added (${record.name})`);
-        }
-      })
-    );
-
-    console.log(`Deleted domain name servers`);
-
-    // subscribe for further changes
-    const janusStream = await janusWorkspaceService.getNodeUpdatesStream();
-    if (janusStream) {
-      const jsonJanusStream = new JSONStream();
-      janusStream.pipe(jsonJanusStream);
-      jsonJanusStream.on('data', object => {
-        console.log('Event: ', JSON.stringify(object, null, 2));
-      });
-      jsonJanusStream.on('error', err => {
-        console.error(err);
-        process.exit(1);
-      });
-    }
-
-    console.log(`Bind handlers on stream data from janus nodes`);
+    let processing = false;
+    setTimeout(async () => {
+      if (!processing) {
+        processing = true;
+        await syncDomainNamesWithJanusNodes(janusWorkspaceService);
+        processing = false;
+      }
+    }, SYNC_INTERVAL);
 
     server.start();
     

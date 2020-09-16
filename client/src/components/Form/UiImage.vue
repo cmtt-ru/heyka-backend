32565bf77260d875322a491320b1499ab0468594
Michael Nalbandyan <xinger@ya.ru>
2020-09-16 11:28:32 +0300

    Update UiImage.vue

--- client/src/components/Form/UiImage.vue
+++ client/src/components/Form/UiImage.vue
@@ -50,24 +50,24 @@ export default {
 
   props: {
     /**
-       * image url
-       */
-    value: {
+     * image url
+     */
+    image: {
       type: String,
       default: '',
     },
 
     /**
-       * image size
-       */
+     * image size
+     */
     size: {
       type: Number,
       default: 40,
     },
 
     /**
-       * Make whole image inactive
-       */
+     * Make whole image inactive
+     */
     disabled: {
       type: Boolean,
       default: false,
@@ -77,30 +77,20 @@ export default {
   data() {
     return {
       tempSrc: null,
+      localImage: null,
     };
   },
 
-  computed: {
-    /**
-       * Local copy of image url
-       * @returns {string} value
-       */
-    localImage: {
-      get() {
-        return this.value;
-      },
-      set(image) {
-        this.$emit('input', image);
-      },
-    },
+  mounted() {
+    this.localImage = this.image;
   },
 
   methods: {
     /**
-       * Trigger after file was selected
-       * @param {object} event - input event
-       * @returns {void}
-       */
+     * Trigger after file was selected
+     * @param {object} event - input event
+     * @returns {void}
+     */
     async storeImageFile(event) {
       if (event.target.files.length !== 1) {
         return;
@@ -113,11 +103,13 @@ export default {
       const formData = new FormData();
 
       formData.append('image', event.target.files[0]);
+
       try {
         this.localDisplayImage(event.target.files[0]);
         const result = await this.$API.user.image(formData);
 
-        this.localImage = result.image;
+        this.localImage = this.tempSrc;
+        this.$emit('input', result.fileId);
       } catch (err) {
         this.tempSrc = null;
         console.log(err);
@@ -125,10 +117,10 @@ export default {
     },
 
     /**
-       * Show newly selected file locally - even before we uploaded it to servers
-       * @param {File} file - image to display
-       * @returns {void}
-       */
+     * Show newly selected file locally - even before we uploaded it to servers
+     * @param {File} file - image to display
+     * @returns {void}
+     */
     localDisplayImage(file) {
       const reader = new FileReader();
 
@@ -139,9 +131,9 @@ export default {
     },
 
     /**
-       * Show "file too big" notification if... file is too big
-       * @returns {void}
-       */
+     * Show "file too big" notification if... file is too big
+     * @returns {void}
+     */
     async tooBigImageAlert() {
       const notification = {
         data: {
@@ -150,7 +142,7 @@ export default {
         },
       };
 
-      await this.$store.dispatch('addNotification', notification);
+      await this.$store.dispatch('app/addNotification', notification);
     },
   },
 
@@ -158,54 +150,54 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-  .image
-    position relative
-    flex-shrink 0
-    border-radius 50%
-    overflow hidden
+.image
+  position relative
+  flex-shrink 0
+  border-radius 50%
+  overflow hidden
 
-    & .input-group
-      position absolute
-      width 100%
-      height 100%
-      top 0
-      left 0
-
-      & .label
-        width 100%
-        height 100%
-        display flex
-        flex-direction column
-        justify-content center
-        align-items center
-        cursor pointer
-        background-color rgba(0, 0, 0, 0.5)
-        color white
-        font-size 12px
-        opacity 0
-        border-radius 50%
-        transition 0.2s opacity ease
-
-        &:hover
-          opacity 1
-
-        &__icon
-          margin-top -4px
-
-  .temp-image
+  & .input-group
     position absolute
+    width 100%
+    height 100%
     top 0
     left 0
-    object-fit cover
-    background-color var(--app-bg)
-    filter grayscale(100%) blur(2px)
 
-  input
-    pointer-events none
-    user-select none
-    outline 0
-    opacity 0
-    width 0
-    height 0
+    & .label
+      width 100%
+      height 100%
+      display flex
+      flex-direction column
+      justify-content center
+      align-items center
+      cursor pointer
+      background-color rgba(0, 0, 0, 0.5)
+      color white
+      font-size 12px
+      opacity 0
+      border-radius 50%
+      transition 0.2s opacity ease
+
+      &:hover
+        opacity 1
+
+      &__icon
+        margin-top -4px
+
+.temp-image
+  position absolute
+  top 0
+  left 0
+  object-fit cover
+  background-color var(--app-bg)
+  filter grayscale(100%) blur(2px)
+
+input
+  pointer-events none
+  user-select none
+  outline 0
+  opacity 0
+  width 0
+  height 0
 
 </style>

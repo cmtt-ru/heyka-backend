67232ad47fc294638ac4c75b83abddd64bf21a4e
Evgenii K <aksyline@gmail.com>
2020-09-29 12:28:37 +0300

    add stats

---
+++ migrations/1601367310302_add-stats.js
@@ -0,0 +1,26 @@
+/* eslint-disable camelcase */
+'use strict';
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('stats', {
+    id: {
+      type: 'uuid',
+      primaryKey: true
+    },
+    user_id: {
+      type: 'uuid',
+      references: 'users(id)',
+      onDelete: 'cascade'
+    },
+    mean_bitrate: 'real',
+    janus_server_url: 'varchar(255)',
+    connection_info: 'jsonb',
+    created_at: 'timestamptz',
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('users', { cascade: true });
+};

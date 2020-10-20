c2b3419b5d07052f85114e64490da9586004d931
Evgenii K <aksyline@gmail.com>
2020-10-20 17:40:32 +0300

    fix for local development

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ module.exports = {
   jwtSecret: process.env.JWT_SECRET || 'default_jwt_token',
   publicHostname: process.env.DEPLOYMENT_ENV === 'stage'
     ? 'heyka.app'
-    : process.env.DEPLOYMENT_ENV ? 'web-dev.dev.k8s.heyka.io' : 'localhost:5000',
+    : process.env.DEPLOYMENT_ENV ? 'web-dev.dev.k8s.heyka.io' : 'localhost:8080',
   leonardo: {
     uploadUrl: process.env.LEONARDO_UPLOAD_URL || 'https://leonardo-direct.osnova.io/upload/files/',
     staticServerUrl: process.env.LEONARDO_STATIC_SERVER_URL || 'https://leonardo.osnova.io/',

ba6214c073416ea22c61e4a5ef18579ddf0dae1a
Evgenii K <aksyline@gmail.com>
2021-03-25 13:41:07 +0300

    Fixes

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const socket = require('./lib/socket');
 async function startServer () {
   const server = await createServer();
   const socketIO = await socket.getSocketIO(server);
-  socketIO.attach(server.listener);
+  socketIO.attach(server.listener, {
+    pingInterval: 25000,
+    pingTimeout: 15000,
+  });
   server.start();
   server.log(['info'], 'Server started');
 }

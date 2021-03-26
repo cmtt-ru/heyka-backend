8b63343557a4a53ce2b0aae7757bfd59aeac5d47
Evgenii K <aksyline@gmail.com>
2021-03-26 13:58:28 +0300

    added comments

--- commit-merge-deploy.sh
+++ commit-merge-deploy.sh
@@ -5,6 +5,6 @@ git add . --all
 git commit -m "$2"
 git checkout $1
 git pull origin $1
-git merge $BRANCH -m "merge ${$1} with ${BRANCH}"
+git merge $BRANCH -m "merge ${1} with ${BRANCH}"
 git push origin $1
 git checkout $BRANCH

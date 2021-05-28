e47643b747197a1a83740f44d3e4de4072be6e31
Evgenii K <aksyline@gmail.com>
2021-05-28 08:08:16 +0300

    sync redis and janus channels

--- server.js
+++ server.js
@@ -11,11 +11,25 @@ module.exports = async function createServer () {
   const server = await Glue.compose(manifest, options);
 
   //// FIX before deploy parallel nodes
+  // sync janus with redis
+  const { janusWorkspaceService } = server.services();
   const db = server.plugins['hapi-pg-promise'].db;
   const redis = server.redis.client;
   const channels = await db.any('SELECT * FROM channels');
-  await Promise.all(channels.map(ch => redis.del(`channel:janus:${ch.id}`)));
+  await Promise.all(channels.map(async ch => {
+    let janusOpts4Channel = await redis.get(`channel:janus:${ch.id}`);
+    if (!janusOpts4Channel) return;
+    janusOpts4Channel = JSON.parse(janusOpts4Channel);
+    await janusWorkspaceService.addAuthTokenForWorkspace(ch.id, janusOpts4Channel);
+    janusOpts4Channel.authToken = ch.id;
+    const roomExists = await janusWorkspaceService.roomExists(ch.janus, janusOpts4Channel);
+    await janusWorkspaceService.deleteAuthTokenForWorkspace(ch.id, janusOpts4Channel);
+    if (!roomExists) {
+      await redis.del(`channel:janus:${ch.id}`);
+    }
+  }));
   console.log(`Delete redis janus opts for ${channels.length} channels`);
+  /*----------*/
 
   return server;
 };

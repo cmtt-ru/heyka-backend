ab9e1b7a58f4d8a9f7175250e4d503db9f39d893
Yaroslav <semyarvlad@gmail.com>
2021-07-06 13:39:13 +0300

    set locale from headers

--- index.js
+++ index.js
@@ -6,12 +6,17 @@ const socket = require('./lib/socket');
 async function startServer () {
   const server = await createServer();
   const socketIO = await socket.getSocketIO(server);
+
   socketIO.attach(server.listener, {
     pingInterval: 25000,
     pingTimeout: 15000,
   });
+
   server.start();
+
+  console.log(`Server running at: ${server.info.uri}`);
   server.log(['info'], 'Server started');
+
 }
 
 startServer();
